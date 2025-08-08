75249c4c4eda95f673bb415103a2e98b323bc323
muneraisa <muneeraisa33@gmail.com>
2025-08-08 14:22:27 +0300

    auth service

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        {/* Non-user routes (available only to guests) */}
+        <Route path="/sign-up" element={<SignUpForm />} />
+        {/* <Route path='/sign-in' element={<SignInForm />} /> */}
       </Routes>
     </>
   );
